fb2467363b2c2498d43d29d4832497f84a43372a
NikiStanchev <nikolay_stanchev@mail.com>
2017-04-18 23:41:53 +0300

    Begin with the project. Created Article Model, but 2 more Properties muss be done.

---
+++ Software Technologies - Blog/models/Article.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+let articleSchema = mongoose.Schema({
+    title:{ type:String, required:true },
+    content:{ type:String, required:true},
+    author:{ type:ObjectId, required:true, ref:'User'},
+    date:{ type:Date, default:Date.now()}
+});
+
+//create the Model in the DB
+const Article = mongoose.model('Article', articleSchema);
+module.exports = Article;
