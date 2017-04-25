54bdf9d923cb7dd14260b6ec1f5591019944f588
NikiStanchev <nikolay_stanchev@mail.com>
2017-04-25 22:27:20 +0300

    HTML updated, new Bootstrap, content of Content and Products limited

--- Software Technologies - Blog/controllers/home.js
+++ Software Technologies - Blog/controllers/home.js
@@ -4,8 +4,23 @@ module.exports = {
   index: (req, res) => {
 
 
-      Article.find({}).limit(6).populate('author').then(articles =>{
+      Article.find({}).populate('author').then(articles =>{
           res.render('home/index', {articles});
+
+          //set the length of the content to maximal 100 symbols
+          articles.forEach(function(article) {
+              if(article.content.length > 100){
+                  article.content = article.content.substring(0,100) + ' ...';
+              }
+          });
+
+          //set the length of the products to maximal 35 symbols
+          articles.forEach(function(article) {
+              if(article.products.length > 35){
+                  article.products = article.products.substring(0,35) + ' ...';
+              }
+          });
+
       });
   }
 };
--- Software Technologies - Blog/models/Article.js
+++ Software Technologies - Blog/models/Article.js
@@ -3,9 +3,11 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 let articleSchema = mongoose.Schema({
     title:{ type:String, required:true },
-    content:{ type:String, required:true},
-    author:{ type:ObjectId, required:true, ref:'User'},
-    date:{ type:Date, default:Date.now()}
+    content:{ type:String, required:true },
+    author:{ type:ObjectId, required:true, ref:'User' },
+    date:{ type:Date, default:Date.now() },
+    time:{ type:String, required:true},
+    products:{ type:String, required:true }
 });
 
 //create the Model in the DB
