63ea9e814ea7f60bece10e1f434e2dc6fec49d33
NikiStanchev <nikolay_stanchev@mail.com>
2017-12-31 08:42:09 +0200

    Angular Exam Project

--- AngularFundamentals/RecipeBlog/recipe-blog/src/users/register-user.model.ts
+++
@@ -1,9 +0,0 @@
-export class RegisterUser{
-    constructor(
-        public email?:string,
-        public password?:string,
-        public firstName?:string,
-        public lastsName?:string,
-        
-    ){}
-}
